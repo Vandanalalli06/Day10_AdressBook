e8d28bb5575f9efc75ab4812d9af267ddba9dbe6
Vandanalalli06 <vandanalalli06@gmail.com>
2022-08-04 16:24:33 +0530

    Welcome to Address Book Problem

---
+++ Day10_AddressBook/Day10_AddressBook/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Day10_AddressBook
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Contact contact = new Contact();
+            Console.WriteLine("Welcome to Adress Book");
+        }
+
+    }
+}
+  
